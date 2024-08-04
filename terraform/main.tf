d6cdee8a8ed1f2facbd8f749116c329c17ef3c9f
calx <108835591+null-calx@users.noreply.github.com>
2024-08-04 13:54:34 +0530

    Create dynamodb

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,3 +27,9 @@ provider "cloudflare" {
 resource "aws_s3_bucket" "terraform_state" {
   bucket = "terraform-estate"
 }
+
+resource "aws_dynamodb_table" "terraform_state_lock" {
+  name           = "terraform-estate"
+  read_capacity  = 1
+  write_capacity = 1
+}
